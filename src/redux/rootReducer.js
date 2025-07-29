a59cce33c5e97afad78bba1d24b1e9afc75cf7b4
Mox Shah <mox@evincedev.com>
2025-07-29 19:31:25 +0530

    remove ecommerce module

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -4,7 +4,6 @@ import layout from './layout'
 import auth from './authentication'
 import users from '@src/views/apps/user/store'
 import invoice from '@src/views/apps/invoice/store'
-import ecommerce from '@src/views/apps/ecommerce/store'
 import dataTables from '@src/views/tables/data-tables/store'
 import permissions from '@src/views/apps/roles-permissions/store'
 
@@ -14,7 +13,6 @@ const rootReducer = {
   navbar,
   layout,
   invoice,
-  ecommerce,
   dataTables,
   permissions
 }
